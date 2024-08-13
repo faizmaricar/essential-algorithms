2a2021e22d9d439159586db7b45b14acf394ab87
Faiz Maricar <maricar.faiz@gmail.com>
2024-08-13 14:41:41 +0800

    setup seed in test

--- problems/p1.spec.js
+++ problems/p1.spec.js
@@ -1,10 +1,7 @@
 import p1Solution from "./p1-solution.js";
 import p1 from "./p1.js";
 
-console.log(process.argv);
-const [, , isSolution] = process.argv;
-
-const func = isSolution ? p1Solution : p1;
+const func = global.isSolution ? p1Solution : p1;
 
 describe("Problem 1: Move zeroes to the end", () => {
   test.each([
