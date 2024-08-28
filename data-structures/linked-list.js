587b55e0ceda01e4da3301dc7d0e8fc7b94a614f
Faiz Maricar <maricar.faiz@gmail.com>
2024-08-28 16:43:30 +0800

    linked list insertAt

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -1,4 +1,35 @@
+import Node from "./node";
+
 export default function LinkedList() {
   this.head = null;
   this.size = 0;
 }
+
+LinkedList.prototype.append = function (val) {
+  let node = new Node(val);
+  let curr = this.head;
+  if (curr === null) this.head = node;
+  else {
+    while (curr.next) {
+      curr = curr.next;
+    }
+    curr.next = node;
+  }
+  this.size++;
+};
+
+LinkedList.prototype.insertAt = function (val, index) {
+  let newNode = new Node(val);
+  let curr = this.head;
+
+  while (--index) {
+    if (curr !== null) curr = curr.next;
+    else throw new Error("Index out of bounds");
+  }
+
+  let nextNode = curr.next;
+  curr.next = newNode;
+  newNode.next = nextNode;
+
+  this.size++;
+};
--- data-structures/linked-list.spec.js
+++ data-structures/linked-list.spec.js
@@ -1,10 +1,46 @@
 import LinkedList from "./linked-list";
-import Node from "./node";
 
 describe("Linked List", () => {
   const list = new LinkedList();
   test("no properties", () => {
     expect(list.head).toBe(null);
     expect(list.size).toBe(0);
+  });
+
+  test("append method assigns added node to head of list", () => {
+    list.append(1);
+    expect(list.head).not.toBeNull();
+    expect(list.head.val).toBe(1);
+    expect(list.size).toBe(1);
+  });
+
+  test("append method add node to the end of the list", () => {
+    let values = [12, 54, 89, 23];
+
+    for (let value of values) list.append(value);
+    let curr = list.head.next;
+
+    for (let value of values) {
+      expect(curr.val).toBe(value);
+      curr = curr.next;
+    }
+    expect(list.size).toBe(values.length + 1);
+  });
+
+  test("insertAt method adds node to position", () => {
+    let val = 63,
+      index = 2;
+
+    list.insertAt(val, index);
+
+    let curr = list.head;
+
+    for (let i = 0; i < index; i++) curr = curr.next;
+
+    expect(curr.val).toBe(val);
+  });
+
+  test("inserAt method throws error if position does not exists", () => {
+    expect(() => list.insertAt(34, 15)).toThrow("Index out of bounds");
   });
 });
